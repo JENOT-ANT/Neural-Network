de358840f2d5de696004b47080ca7fe6facbd57f
JENOT-ANT <antekswitaj@outlook.com>
2023-12-14 01:24:13 +0100

    Update README.md, small readability improvements in the adjust-sinus.py

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Neural-Network
-A module intended for building and teaching neural networks.
+An **object-oriented** python module intended for building and teaching rather small neural networks.
+
+
+# Usage
+**In case anybody would like to use the code:**
+> I recomend simply copying only the main module file - `neural_network.py` into your project's **root folder**, and importing just the right staff in your scripts, for example like that:
+
+```py
+from neural_network import Network, soft_max
+```
+
+
+# Examples
+- To run example scripts, you can use folowing syntax **(while in project's directory)**:
+
+```sh
+python3 -m examples.<example_name_without_file_extension>
+```
+
+- `adjust-sinus.py` script requires `turtle` module to draw the resoult.
+- If you get bored with waiting for the AI to learn more, consider clicking `Ctrl + c` to finish the script and to see the best resoult found at that point.
+- At the top part of the example code, you can edit parameters of the math-function f(x) to be drawn.
--- examples/adjust-sinus.py
+++ examples/adjust-sinus.py
@@ -2,16 +2,16 @@ from neural_network import Network
 from turtle import Turtle, mainloop
 from math import sin, pi
 
-# FUNCTION INFO:
+# MATH FUNCTION INFO:
 FUNC = sin
 DOMAIN: tuple = (0, 2 * pi)
-STEP: float = pi / 12
-V_RANGE: tuple = (-1, 1)
+SET_OF_VALUES: tuple = (-1, 1)
+STEP: float = pi / 24
 
 # NETWORK CONFIG:
 HIDDEN_LAYERS: int = 2
 NEURONS_PER_LAYER: int = 8
-
+TARGET_LOSS: float = 0.025
 
 def main():
     ai = Network(HIDDEN_LAYERS, NEURONS_PER_LAYER, 1, 1)
@@ -22,33 +22,37 @@ def main():
     min_loss: float = 1e6
 
     try:
-        while min_loss > 0.005:
+        while min_loss > TARGET_LOSS:
             for layer_index in range(HIDDEN_LAYERS + 1):
-                for q in range(1, 51):
-                    ai.randomize(q * 0.001, layer_index)
-                    loss = 0
-                    x = DOMAIN[0]
-                    
-                    while x < DOMAIN[1]:
-                        y = ai.test_compute([x, ], output_activation=lambda output: output)[0]
-                        loss += abs(y - FUNC(x))
-                        x += STEP
-                    
-                    # MEDIUM VALUE OF THE LOSS
-                    loss /= (DOMAIN[1] - DOMAIN[0]) // STEP
-                    # PERCENT OF THE FUCTION'S RANGE
-                    loss /= (V_RANGE[1] - V_RANGE[0])
+                # for q in range(1, 11):
+                # ai.randomize(q * 0.001, layer_index)
+                ai.randomize(0.004, layer_index)
+                loss = 0
+                x = DOMAIN[0]
+                
+                while x < DOMAIN[1]:
+                    y = ai.test_compute([x, ], output_activation=lambda output: output)[0]
+                    loss += abs(y - FUNC(x))
+                    x += STEP
+                
+                # Medium value of the loss per one step
+                loss /= (DOMAIN[1] - DOMAIN[0]) // STEP
+                # Converting to a percent of the function's set of values
+                loss /= (SET_OF_VALUES[1] - SET_OF_VALUES[0])
 
-                    if loss < min_loss:
-                        ai.apply()
-                        min_loss = loss
-                        print(f'{layer_index}-{q}: {round(min_loss * 100, 1)} %')
+                # If better adjustment found:
+                if loss < min_loss:
+                    ai.apply()
+                    min_loss = loss
+                    # print(f'[{layer_index}-{q}] Loss: {round(min_loss * 100, 1)} %')
+                    print(f'[{layer_index}] Loss: {round(min_loss * 100, 1)} %')
+    
     except:
         pass
         
 
     turtle = Turtle('turtle')
-    turtle.speed(2)
+    turtle.speed(0)
 
     # DRAW FUNCTION
     turtle.color('green')
@@ -56,8 +60,8 @@ def main():
     turtle.goto(-200, 0)
     turtle.down()
 
-    x = 0
-    while x < 2 * pi:
+    x = DOMAIN[0]
+    while x < DOMAIN[1]:
         y = FUNC(x)
 
         turtle.goto(-200 + x * 50, y * 50)
@@ -69,12 +73,12 @@ def main():
     turtle.goto(-200, 0)
     turtle.down()
 
-    x = 0
-    while x < 2 * pi:
+    x = DOMAIN[0]
+    while x < DOMAIN[1]:
         y = ai.compute([x, ], output_activation=lambda output: output)[0]
 
         turtle.goto(-200 + x * 50, y * 50)
-        x += pi / 12
+        x += STEP
     
     mainloop()
 
